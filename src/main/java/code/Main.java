774b12f46f2aed8ea842a7b20ac1d6b46ec0938c
kenneth <785172550@qq.com>
2020-02-12 12:18:16 +0800

    add java class path out

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
   public static void main(String[] args) {
+    log.info("App class path:\n{}", System.getProperty("java.class.path").replace(";", "\n"));
     log.info("main test");
   }
 }
